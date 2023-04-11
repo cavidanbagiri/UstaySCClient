4d96ea501c04c00a564c5729a233591e70045a66
CavidanBagirliCamal <cbtechauto@gmail.com>
2023-04-11 14:35:02 +0300

    Fiing Table col size

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 
 <template>
-  <div class="">
+  <div class="p-1 w-full">
     <STF_COMP/>
   </div>
 </template>
 
 <script setup>
-  import STF_COMP from './components/tables/STF_COMP.vue';
+  import STF_COMP from './components/tables/STF_Table_Comp.vue';
 </script>
 
 <style lang="">
