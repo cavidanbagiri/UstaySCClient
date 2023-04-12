4ecf21f52861d950c7f2d3315f2574725f958127
CavidanBagirliCamal <cbtechauto@gmail.com>
2023-04-12 16:02:36 +0300

    Fixing Design

--- src/layouts/Navbar.vue
+++ src/layouts/Navbar.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <div class="grid grid-cols-12 p-1 bg-slate-800 color-white">
+    <div class="grid grid-cols-12  bg-slate-800 color-white">
         <div class="col-span-3 text-white flex items-center" >
             <img class="w-16 h-12" src="https://cdn2.iconfinder.com/data/icons/letters-and-numbers-1/32/lowercase_letter_u_red-512.png" alt="">
         </div>
@@ -17,9 +17,10 @@
             <span class="p-2 rounded-xl cursor-pointer hover:bg-black" ><i class="fa-regular fa-bell fa-md " style="color:white"></i></span>
             <span class="p-2 rounded-xl cursor-pointer hover:bg-black" ><i class="fa-regular fa-message fa-md" style="color:white"></i></span>
             <span class="p-2 rounded-xl cursor-pointer hover:bg-black" ><i class="fa-solid fa-briefcase fa-md" style="color:white"></i></span>
-            <span class="p-2 rounded-xl cursor-pointer hover:bg-black">
-                <i class="fa-regular fa-user fa-md mr-1" style="color:white"></i>
-                <span class=" text-white text-sm">Lorem Ipsum</span>
+            <span class="p-2 rounded-xl cursor-pointer hover:bg-black flex flex-row items-center">
+                <!-- <i class="fa-regular fa-user fa-md mr-1" style="color:white"></i> -->
+                <img class="w-10 h-10 rounded-full" src="https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5377.jpg?w=2000" alt="">
+                <span class=" text-sm text-slate-300">Cavidan Bagirli</span>
             </span>
             <span class="py-1 px-2 rounded-xl cursor-pointer hover:bg-black"><i class="fa-solid fa-ellipsis-vertical fa-md" style="color: white;"></i></span>
         </div>
