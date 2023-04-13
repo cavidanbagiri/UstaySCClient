bed589432c468764fe4f0d0acfdb9ed13fd0d55c
CavidanBagirliCamal <cbtechauto@gmail.com>
2023-04-13 14:15:13 +0300

    Create MTF COmp Table Design

--- src/components/MTF_Table_Comp.vue
+++ src/components/MTF_Table_Comp.vue
@@ -1,7 +1,21 @@
 
 <template>
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div class="relative overflow-x-auto shadow-xl sm:rounded-lg border p-2">
+        <!-- MTF Title Section -->
+        <div class="text-center bg-gray-50">
+            <span class="py-5 font-bold text-2xl my-5 ">Creating MTF</span>
+            <hr>
+        </div>
+        <!-- MTF Inform Section -->
+        <div class="flex flex-row justify-around p-2 border my-1 text-gray-200 rounded-md bg-slate-800">
+            <span class="text-xs font-mono font-medium" ><span class="font-bold">MTF Number</span> : SRU.RS07.0001</span>
+            <span class="text-xs font-mono font-medium" ><span class="font-bold">Created Date</span> : 13-04-2023</span>
+            <span class="text-xs font-mono font-medium" ><span class="font-bold">Project</span> : Ustay-SRU</span>
+            <span class="text-xs font-mono font-medium" ><span class="font-bold">Order</span> : Cavidan Bagirli</span>
+        </div>
+        <!-- Table Section -->
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            
             <!-- Table Header -->
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
@@ -28,6 +42,9 @@
                     <th scope="col" class="px-6 py-1 font-mono font-medium text-center border w-8">
                         Field
                     </th>
+                    <th scope="col" class="px-6 py-1 font-mono font-medium text-center border w-8">
+                        Status
+                    </th>
                     <th scope="col" class="px-6 py-1 font-mono font-medium text-center border">
                         Comment
                     </th>
@@ -37,14 +54,19 @@
 
             <!-- Table Body -->
             <tbody>
-
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <tr v-for="i in row_size" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ">
                     
-                    <th scope="row" class="w-8 p-1 font-mono font-thin border text-gray-900 whitespace-nowrap dark:text-white text-center" style="font-size: smaller;">
-                        <span>1</span>
+                    <th scope="row" class="w-8 font-mono font-thin text-gray-900 whitespace-nowrap dark:text-white text-center" style="font-size: smaller;">
+                        <span>{{ i }}</span>
                     </th>
                     <th scope="row" class="p-1 font-mono font-thin border text-gray-900 whitespace-nowrap dark:text-white" style="font-size: smaller;">
-                        <input class="border outline-none font-sans rounded-lg w-full h-full p-2" type="text" placeholder="Material Type...">
+                        <!-- <input class="border outline-none font-sans rounded-lg w-full h-full p-2" type="text" placeholder="Material Type..."> -->
+                        <select class="border outline-none font-sans rounded-lg w-full h-full p-2" v-model="selected" >
+                            <option disabled value="">Select Type</option>
+                            <option>Project</option>
+                            <option>Consumables</option>
+                            <option>Fixture</option>
+                        </select>
                     </th>
                     <td class="p-1 font-mono font-thin border text-gray-900 whitespace-nowrap dark:text-white" style="font-size: smaller;">
                         <input class="border outline-none font-sans rounded-lg w-full h-full p-2" type="text" placeholder="Material Name...">
@@ -53,23 +75,83 @@
                         <input class="border outline-none font-sans rounded-lg w-full h-full p-2" type="text" placeholder="Count...">
                     </td>
                     <td class="p-1 border" style="font-size: smaller;">
-                        <input class="border outline-none font-sans rounded-lg w-full h-full p-2 " type="text" placeholder="Unit...">
+                        <!-- <input class="border outline-none font-sans rounded-lg w-full h-full p-2 " type="text" placeholder="Unit..."> -->
+                        <select class="border outline-none font-sans rounded-lg w-full h-full p-2" v-model="selected" >
+                            <option disabled value="">Unit</option>
+                            <option>Adet</option>
+                            <option>M2</option>
+                            <option>M3</option>
+                            <option>Ton</option>
+                            <option>Kg</option>
+                        </select>
                     </td>
                     <td class="p-1 border" style="font-size: smaller;">
-                        <input class="border outline-none font-sans rounded-lg w-full h-full p-2" type="text" placeholder="Field...">
+                        <!-- <input class="border outline-none font-sans rounded-lg w-full h-full p-2" type="text" placeholder="Field..."> -->
+                        <select class="border outline-none font-sans rounded-lg w-full h-full p-2" v-model="selected" >
+                            <option disabled value="">Field</option>
+                            <option>PR-2</option>
+                            <option>Derjisnkoe</option>
+                            <option>PR-6</option>
+                        </select>
+                    </td>
+                    <td class="p-1 border" style="font-size: smaller;">
+                        <!-- <input class="border outline-none font-sans rounded-lg w-full h-full p-2" type="text" placeholder="Status..."> -->
+                        <select class="border outline-none font-sans rounded-lg w-full h-full p-2" v-model="selected" >
+                            <option disabled value="">Status</option>
+                            <option>
+                                <span>
+                                    <i class="fa-solid fa-gem fa-lg" style="color:red"></i> Urgent
+                                </span>
+                            </option>
+                            <option>
+                                <span>
+                                    <i class="fa-solid fa-gem fa-lg" style="color:red"></i> Normally
+                                </span>
+                            </option>
+                        </select>
                     </td>
                     <td class="p-1 border" style="font-size: smaller;">
                         <input class="border outline-none font-sans rounded-lg w-full h-full p-2" type="text" placeholder="Comment...">
                     </td>
 
                 </tr>
-
             </tbody>
         </table>
+        
+        <!-- Add New Column -->
+        <div class="flex justify-between">
+            <button @click="addNewColumn" class=" font-bold py-2 px-2 my-1 text-xs outline-none font-mono  border text-gray-100 rounded-xl bg-orange-500">
+                <i class="fa-solid fa-plus fa-lg"></i> 
+                Add New Column
+            </button>
+            <button @click="addNewColumn" class="  py-2 px-2 my-1 text-xs outline-none font-mono font-bold border text-gray-100 rounded-xl bg-red-500">
+                <i class="fa-solid fa-minus fa-lg"></i>
+                Remove Column
+            </button>
+        </div>
+
+        <!-- Creaate MTF Button-->
+        <div class="flex items-center justify-end">
+            <span class="p-3">Create MTF</span>
+            <button @click="addNewColumn" class="  py-1 px-2 my-1 text-lg w-12 h-12 outline-none font-mono font-bold border text-gray-100 rounded-full bg-green-400">
+                <i class="fa-solid fa-plus fa-xl"></i>
+            </button>
+        </div>
+
     </div>
 </template>
 
 <script setup>
+
+    import {ref} from 'vue';
+
+    const selected = ref('');
+
+    const row_size = ref(5);
+
+    const addNewColumn = () => {
+        row_size.value+=1;
+    }
 
 </script>
 
--- src/layouts/Navbar.vue
+++ src/layouts/Navbar.vue
@@ -3,11 +3,11 @@
     <div class="grid grid-cols-12  bg-slate-800 color-white">
         <div class="col-span-3 text-white flex flex-row items-center justify-evenly" >
             <router-link to="/">
-                <img class="w-16 h-12" src="https://cdn2.iconfinder.com/data/icons/letters-and-numbers-1/32/lowercase_letter_u_red-512.png" alt="">
+                <img class="w-12 h-12" src="https://cdn2.iconfinder.com/data/icons/letters-and-numbers-1/32/lowercase_letter_u_red-512.png" alt="">
             </router-link>
             <span class="p-2 rounded-xl cursor-pointer hover:bg-black flex flex-row items-center">
                 <!-- <i class="fa-regular fa-user fa-md mr-1" style="color:white"></i> -->
-                <img class="w-10 h-10 rounded-full" src="https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5377.jpg?w=2000" alt="">
+                <img class="w-8 h-8 rounded-full" src="https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5377.jpg?w=2000" alt="">
                 <span class=" text-sm text-slate-300">Cavidan Bagirli</span>
             </span>
         </div>
@@ -21,7 +21,7 @@
         
         <!-- User Side -->
         <div class="col-span-3 flex flex-row items-center justify-evenly" >
-            
+                
             <span class="p-2 rounded-xl cursor-pointer w-10 h-10 flex flex-row justify-center items-center hover:bg-black" ><i class="fa-regular fa-bell fa-md " style="color:white"></i></span>
         
             <span class="p-2 rounded-xl cursor-pointer w-10 h-10 flex flex-row justify-center items-center hover:bg-black" ><i class="fa-regular fa-message fa-md" style="color:white"></i></span>
