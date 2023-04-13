d54b1f1ca2342af39c3260e449f67adfac53b9e6
CavidanBagirliCamal <cbtechauto@gmail.com>
2023-04-13 14:43:50 +0300

    Fixing Table ping animation

--- src/components/MTF_Table_Comp.vue
+++ src/components/MTF_Table_Comp.vue
@@ -21,7 +21,7 @@
                 <tr>
                     
                     <th scope="col" class="px-6 py-1 font-mono font-medium text-center border">
-                        S/S
+                        S
                     </th>
                     <th scope="col" class="px-6 py-1 font-mono font-medium text-center border">
                         <div class="w-18">
@@ -57,7 +57,13 @@
                 <tr v-for="i in row_size" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ">
                     
                     <th scope="row" class="w-8 font-mono font-thin text-gray-900 whitespace-nowrap dark:text-white text-center" style="font-size: smaller;">
-                        <span>{{ i }}</span>
+                        <div class="flex justify-between items-center px-1">
+                            <span>{{ i }}</span>
+                            <span class="relative flex h-3 w-3">
+                                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-orange-400 opacity-75"></span>
+                                <span class="relative inline-flex rounded-full h-3 w-3 bg-orange-500"></span>
+                            </span>
+                        </div>
                     </th>
                     <th scope="row" class="p-1 font-mono font-thin border text-gray-900 whitespace-nowrap dark:text-white" style="font-size: smaller;">
                         <!-- <input class="border outline-none font-sans rounded-lg w-full h-full p-2" type="text" placeholder="Material Type..."> -->
