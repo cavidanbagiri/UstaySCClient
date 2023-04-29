dfd567845c2cde9d22e97b088b422012a8bb4930
CavidanBagirliCamal <cbtechauto@gmail.com>
2023-04-29 14:40:11 +0300

    change btn and add row html

--- src/components/design/Dropdown_Comp.vue
+++ src/components/design/Dropdown_Comp.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="border-2 p-3 ">
+        {{ selected_user }}
+        <button v-html="selected_user" class="border py-2 px-6 w-60 bg-blue-500 text-white font-bold"
+            @click="show_dropdown = !show_dropdown">
 
-        <button class="border py-2 px-6 w-60 bg-blue-500 text-white font-bold" @click="show_dropdown = !show_dropdown">
-            Open
         </button>
 
         <transition enter-active-class="transition ease-out duration-750" enter-class="transform opacity-0 scale-95"
@@ -11,15 +12,18 @@
 
             <!-- Dropdown menu -->
             <div id="dropdownUsers" v-if="show_dropdown" class="z-10 bg-white rounded-lg shadow w-60 dark:bg-gray-700">
-                <ul class="h-48 py-2  overflow-y-auto text-gray-700 dark:text-gray-200" aria-labelledby="dropdownUsersButton">
-                    <li class="text-xs">
-                        <a href="#"
-                            class="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                            <img class="w-6 h-6 mr-2 rounded-full"
-                                src="https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5377.jpg?w=2000"
-                                alt="Jese image">
-                            Cavidan Bagirli
-                        </a>
+                <ul class="h-48 py-2  overflow-y-auto text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdownUsersButton">
+                    <li class="text-xs" @click="changeBtnText($event)">
+                        <div class="">
+                            <a href="#"
+                                class="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                <img class="w-6 h-6 mr-2 rounded-full"
+                                    src="https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5377.jpg?w=2000"
+                                    alt="Jese image">
+                                Cavidan Bagirli
+                            </a>
+                        </div>
                     </li>
                     <li class="text-xs">
                         <a href="#"
@@ -48,7 +52,7 @@
                             Husame Sahin
                         </a>
                     </li>
-    
+
                 </ul>
             </div>
         </transition>
@@ -61,6 +65,20 @@
 <script setup>
 import { ref } from 'vue';
 const show_dropdown = ref(false);
+
+let selected_user = ref('<div class="flex items-center text-xs"><img class="w-6 h-6 mr-2 rounded-full"' +
+    'src="https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5377.jpg?w=2000"' +
+    'alt="Jese image">' +
+    'Supplier Name</div>')
+
+const changeBtnText = (elem) => {
+    console.log(elem.target);
+    // selected_user.value='<div class="flex items-center text-xs"><img class="w-6 h-6 mr-2 rounded-full"' +
+    // 'src="https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5377.jpg?w=2000"' +
+    // 'alt="Jese image">' +
+    // 'Hello world</div>'
+    selected_user.value=elem.srcElement
+}
 
 </script>
 
