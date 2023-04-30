0e80b8743cb748a8463be27c842ccfd62b348672
CavidanBagirliCamal <cbtechauto@gmail.com>
2023-04-30 15:37:35 +0300

    Not Fixed Dropdown Menu

--- src/components/design/Dropdown_Comp.vue
+++ src/components/design/Dropdown_Comp.vue
@@ -1,7 +1,6 @@
 <template>
 
     <div class="border-2 p-3 ">
-        {{ selected_user }}
         <button v-html="selected_user" class="border py-2 px-6 w-60 bg-blue-500 text-white font-bold"
             @click="show_dropdown = !show_dropdown">
 
@@ -11,16 +10,13 @@
             enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-750"
             leave-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95"> -->
 
-            <transition enter-active-class="transition ease-out duration-750" enter-class="transform opacity-0 scale-95"
-            enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-750"
-            leave-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-
+            
             <!-- Dropdown menu -->
             <div id="dropdownUsers" v-if="show_dropdown" class="z-10 bg-white rounded-lg shadow w-60 dark:bg-gray-700">
                 <ul class="h-48 py-2  overflow-y-auto text-gray-700 dark:text-gray-200"
                     aria-labelledby="dropdownUsersButton">
-                    <li class="text-xs" @click="changeBtnText($event)">
-                        <div class="">
+                    <li class="text-xs" >
+                        <div class="" ref="div" @click="changeBtnText($refs)" id="masin">
                             <a href="#"
                                 class="flex items-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                                 <img class="w-6 h-6 mr-2 rounded-full"
@@ -60,7 +56,6 @@
 
                 </ul>
             </div>
-        </transition>
 
     </div>
     
@@ -70,18 +65,16 @@
 import { ref } from 'vue';
 const show_dropdown = ref(false);
 
+const div = ref(null);
+
 let selected_user = ref('<div class="flex items-center text-xs"><img class="w-6 h-6 mr-2 rounded-full"' +
     'src="https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5377.jpg?w=2000"' +
     'alt="Jese image">' +
     'Supplier Name</div>')
 
-const changeBtnText = (elem) => {
-    console.log(elem.target);
-    // selected_user.value='<div class="flex items-center text-xs"><img class="w-6 h-6 mr-2 rounded-full"' +
-    // 'src="https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5377.jpg?w=2000"' +
-    // 'alt="Jese image">' +
-    // 'Hello world</div>'
-    selected_user.value=elem.srcElement
+const changeBtnText = ($elem) => {
+    console.log($elem.div);
+    selected_user.value=$elem.div.value
 }
 
 </script>
