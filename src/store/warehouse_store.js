9dc1b62b31c80adf26cb21d617837be44454452e
cavidanbagiri <cavidanbagiri@gmail.com>
2023-05-06 06:06:13 +0300

    Send Data To Server but need fixed

--- src/store/warehouse_store.js
+++ src/store/warehouse_store.js
@@ -7,10 +7,12 @@ const WarehouseStore = defineStore('WarehouseStore',{
 
     state:()=>({
         waiting_sms : [],
+        accepted_items : [],
     }),
 
     getters:{
         GETWAITINGSMS : (state) => state.waiting_sms,
+        GETACCEPTEDITEMS : (state) => state.accepted_items,
     },
     actions:{
 
@@ -18,7 +20,6 @@ const WarehouseStore = defineStore('WarehouseStore',{
         async getWaitingsSMS () { 
             await axios.get('http://localhost:3000/warehouse/waitingsm')
             .then((respond)=>{
-                console.log('respond is : ',respond.data);
                 this.waiting_sms = respond.data
             }).catch((err)=>{
                 console.log('Get Warehouse Waiting Error : ',err);
@@ -27,7 +28,15 @@ const WarehouseStore = defineStore('WarehouseStore',{
 
         // Post Accepted waiting to Warehouse
         async acceptWaitingSM(items){
-            console.log('items is : ',items.value);
+            // console.log(items.value);
+            await axios.post('http://localhost:3000/warehouse/accept',items.value)
+            .then((respond)=>{
+                this.accepted_items = respond;
+                console.log('accepted items is : ',this.accepted_items);
+            })
+            .catch((err)=>{
+                console.log('Accepted Materials Error From Warehouse Store : ',err);
+            })
         }
 
     }
