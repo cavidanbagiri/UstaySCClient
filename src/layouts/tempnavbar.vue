a2d424a7047b985da54ba87565efd2c3551414a9
cavidanbagiri <cavidanbagiri@gmail.com>
2023-06-02 20:47:11 +0300

    Adding Tooltip To Menu

--- src/layouts/tempnavbar.vue
+++ src/layouts/tempnavbar.vue
@@ -2,57 +2,116 @@
     <div class="flex flex-col h-screen bg-gradient-to-r from-blue-500 to-blue-600 w-14 items-center justify-start p-0 m-0 rounded-r-md 
     border-r-2 ">
         <!-- Open Toggle Canvas -->
-        <span @click="toggleCanvas"
-            class="rounded-md cursor-pointer  mt-5 w-8 h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
-                class="fa-solid fa-bars fa-md" style="color:white;"></i></span>
-        <!-- WorkSpace Menu -->
-        <router-link to="/workspace">
-            <span
+        <div class="relative">
+            <span @click="toggleCanvas" @mouseover="menu_tooltip = true" @mouseleave="menu_tooltip = false"
                 class="rounded-md cursor-pointer  mt-5 w-8 h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
-                    class="fa-solid fa-briefcase fa-md" style="color:white"></i></span>
-        </router-link>
-        <!-- Notification -->
-        <span class="rounded-md cursor-pointer  mt-5 w-8 h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
-                class="fa-regular fa-bell fa-md " style="color:white"></i></span>
+                    class="fa-solid fa-bars fa-md" style="color:white;"></i>
+            </span>
+            <span v-if="menu_tooltip"
+                class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md flex">
+                Catalog
+            </span>
+        </div>
+        <!-- WorkSpace Menu -->
+        <div class="relative">
+            <router-link to="/workspace">
+                <span @mouseover="workspace_tooltip = true" @mouseleave="workspace_tooltip = false"
+                    class="rounded-md cursor-pointer  mt-5 w-8 h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
+                        class="fa-solid fa-briefcase fa-md" style="color:white"></i>
+                </span>
+                <span v-if="workspace_tooltip"
+                    class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md flex">
+                    Workspace
+                </span>
+            </router-link>
+        </div>
 
-        <!-- Message Menu-->
-        <span class="rounded-md cursor-pointer  mt-5 w-8 h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
-                class="fa-regular fa-message fa-md" style="color:white"></i></span>
-
-        <!-- Starred Materials -->
-        <span class="rounded-md cursor-pointer  mt-5 w-8 h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
-                class="fa-regular fa-star fa-md" style="color:white"></i></span>
+        <div class="relative">
+            <router-link to="/">
+                <span @mouseover="notification_tooltip = true" @mouseleave="notification_tooltip = false"
+                    class="rounded-md cursor-pointer  mt-5 w-8 h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
+                        class="fa-regular fa-bell fa-md " style="color:white"></i></span>
+                <span v-if="notification_tooltip"
+                    class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md flex">
+                    Notification
+                </span>
+            </router-link>
+        </div>
+        <div class="relative">
+            <router-link to="/">
+                <span @mouseover="messages_tooltip = true" @mouseleave="messages_tooltip = false"
+                    class="rounded-md cursor-pointer  mt-5 w-8 h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
+                        class="fa-regular fa-message fa-md" style="color:white"></i></span>
+                <span v-if="messages_tooltip"
+                    class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md flex">
+                    Messages
+                </span>
+            </router-link>
+        </div>
+        <div class="relative">
+            <router-link to="/">
+                <span @mouseover="star_tooltip = true" @mouseleave="star_tooltip = false"
+                    class="rounded-md cursor-pointer  mt-5 w-8 h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
+                        class="fa-regular fa-star fa-md" style="color:white"></i></span>
+                <span v-if="star_tooltip"
+                    class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md flex">
+                    Stars
+                </span>
+            </router-link>
+        </div>
 
         <!-- STF Menu -->
-        <router-link to="/order/stf" class="no-underline ">
-            <span
-                class="p-2 rounded-md cursor-pointer  mt-5 w-8  h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
-                    class="fa-solid fa-plus fa-md" style="color:white"></i></span>
-        </router-link>
+        <div class="relative">
+            <router-link to="/order/stf" class="no-underline ">
+                <span @mouseover="stf_tooltip = true" @mouseleave="stf_tooltip = false"
+                    class="p-2 rounded-md cursor-pointer  mt-5 w-8  h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300"><i
+                        class="fa-solid fa-plus fa-md" style="color:white"></i>
+                </span>
+                <span v-if="stf_tooltip"
+                    class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md flex">
+                    Orders
+                </span>
+            </router-link>
+        </div>
         <!-- Warehouse Menu -->
-        <router-link to="/procurement" class="no-underline ">
-            <span
-                class="p-2 rounded-md cursor-pointer  mt-5 w-8  h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300">
-                <i class="fa-regular fa-handshake fa-sm" style="color:white;"></i>
-            </span>
-        </router-link>
+        <div class="relative">
+            <router-link to="/procurement" class="no-underline ">
+                <span @mouseover="procurement_tooltip = true" @mouseleave="procurement_tooltip = false"
+                    class="p-2 rounded-md cursor-pointer  mt-5 w-8  h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300">
+                    <i class="fa-regular fa-handshake fa-sm" style="color:white;"></i>
+                </span>
+                <span v-if="procurement_tooltip"
+                    class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md">
+                    Procurement
+                </span>
+            </router-link>
+        </div>
         <!-- Procurement Menu -->
         <div class="relative">
             <router-link to="/warehouse" class="no-underline ">
-            <span @mouseover="warehouse_tooltip=true" @mouseleave="warehouse_tooltip=false"
-                class="p-2 rounded-md cursor-pointer  mt-5 w-8  h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300">
-                <i class="fa-solid fa-warehouse fa-sm" style="color:white;"></i>
-            </span>
-            <span v-if="warehouse_tooltip" class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md">
-                Warehouse
-            </span>
-        </router-link>
+                <span @mouseover="warehouse_tooltip = true" @mouseleave="warehouse_tooltip = false"
+                    class="p-2 rounded-md cursor-pointer  mt-5 w-8  h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300">
+                    <i class="fa-solid fa-warehouse fa-sm" style="color:white;"></i>
+                </span>
+                <span v-if="warehouse_tooltip"
+                    class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md">
+                    Warehouse
+                </span>
+            </router-link>
         </div>
-        
-        <span
-            class="p-2 rounded-md cursor-pointer  mt-5 w-8  h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300">
-            <i class="fa-regular fa-user fa-sm" style="color:white;"></i>
-        </span>
+
+        <div class="relative">
+            <router-link to="/">
+                <span @mouseover="profile_tooltip = true" @mouseleave="profile_tooltip = false"
+                    class="p-2 rounded-md cursor-pointer  mt-5 w-8  h-8 flex flex-row justify-center items-center hover:bg-blue-800 duration-300">
+                    <i class="fa-regular fa-user fa-sm" style="color:white;"></i>
+                </span>
+                <span v-if="profile_tooltip"
+                    class="duration-150 absolute top-5 left-10 ml-1 bg-slate-800 p-2 text-white text-xs rounded-md">
+                    Profile
+                </span>
+            </router-link>
+        </div>
 
     </div>
 </template>
@@ -69,6 +128,14 @@ const user_store = UserStore();
 
 // Show Tooltips
 const warehouse_tooltip = ref(false)
+const profile_tooltip = ref(false)
+const procurement_tooltip = ref(false)
+const stf_tooltip = ref(false)
+const star_tooltip = ref(false)
+const notification_tooltip = ref(false)
+const messages_tooltip = ref(false)
+const workspace_tooltip = ref(false)
+const menu_tooltip = ref(false)
 
 // Open And Close Canvas
 const toggleCanvas = () => index_store.TOGGLECANVAS()
