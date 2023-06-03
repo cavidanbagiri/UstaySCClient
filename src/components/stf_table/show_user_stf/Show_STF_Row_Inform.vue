34e9c2a45c466b1f19f195931b4bc2920fbf5422
CavidanBagirliCamal <cbtechauto@gmail.com>
2023-06-03 14:26:26 +0300

    Fix Design

--- src/components/stf_table/show_user_stf/Show_STF_Row_Inform.vue
+++ src/components/stf_table/show_user_stf/Show_STF_Row_Inform.vue
@@ -1,12 +1,17 @@
 <template>
-    <div v-if="order_store.GETROWINFORM" class="h-screen w-1/2 bg-red-300 fixed top-0 right-0 z-10">
+    <div v-if="order_store.GETROWINFORM" class="h-screen w-1/2 fixed top-0 right-0 z-10 border bg-white">
         <div class="flex flex-col">
-            <div class="flex justify-end bg-blue-300">
+            <div class="flex justify-end">
                 <button @click="closeRowInform" class="p-2">
-                    X
+                    <i class="fa-solid fa-xmark fa-lg"></i>
                 </button>
             </div>
             <div class="flex flex-col">
+                <div>Created By : </div>
+                <div>Created Time : </div>
+                <div>STF No :  </div>
+                <div>SM No : </div>
+                <div>Condition : </div>
             </div>
         </div>
     </div>
