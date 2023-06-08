33566a9aecf3019f5ff6217e83c3976f2556e72e
cavidanbagiri <cavidanbagiri@gmail.com>
2023-06-09 00:32:47 +0300

    Fix Z Index

--- src/components/order/show_user_stf/Show_STF_Comp.vue
+++ src/components/order/show_user_stf/Show_STF_Comp.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="">
 
-        <div class="h-40 sticky top-10 bg-white w-full z-10">
+        <div class="h-40 sticky top-10 bg-white w-full">
             <Show_STF_Analyz />
 
             <Show_STF_Filter_Section />
--- src/components/order/show_user_stf/Show_STF_Table_Header.vue
+++ src/components/order/show_user_stf/Show_STF_Table_Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <thead class="text-black bg-gray-100 border w-full sticky top-48 z-10" v-font-family
+    <thead class="text-black bg-gray-100 border w-full sticky top-48 " v-font-family
     style="letter-spacing: 0.5px; font-size: x-small;">
         <tr class="">
             <th scope="col-4" class=" px-4">
