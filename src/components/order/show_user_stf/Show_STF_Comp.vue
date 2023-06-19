eb51a53d58c88456dfdae28398528cff5e969a05
cavidanbagiri <cavidanbagiri@gmail.com>
2023-06-19 20:03:11 +0300

    Change px to 5rem

--- src/components/order/show_user_stf/Show_STF_Comp.vue
+++ src/components/order/show_user_stf/Show_STF_Comp.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col" style="display: inline-block;">
 
         <div class="sticky top-10 p-1 ">
-            <div class=" sticky left-16 flex flex-col bg-white" style="display: inline-block; width: calc(100vw - 100px);">
+            <div class=" sticky left-16 flex flex-col bg-white" style="display: inline-block; width: calc(100vw - 5rem);">
                 <!-- <div class="bg-blue-300 p-1 "> -->
 
                         <Show_STF_Analyz />
